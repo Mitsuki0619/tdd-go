c2a4bb6726413d898bb0d2a4b7b4d05405d8769c
Mitsuki0619 <m.iwamura1975@gmail.com>
2025-06-11 17:06:58 +0900

    packaging

---
+++ money-tdd/money/money_test.go
@@ -0,0 +1,38 @@
+package money
+
+import (
+	"testing"
+	"github.com/Mitsuki0619/tdd-go/money-tdd/test_helper"
+)
+
+func TestMultiplication(t *testing.T) {
+	dollar := NewDollar(2)
+	testHelper.AssertEquals(t, dollar.Times(5), NewDollar(10))
+	testHelper.AssertEquals(t, dollar.Times(3), NewDollar(6))
+}
+
+func TestEquality(t *testing.T) {
+	testHelper.AssertTrue(t, NewDollar(3).Equals(NewDollar(3)))
+	testHelper.AssertFalse(t, NewDollar(4).Equals(NewDollar(5)))
+	testHelper.AssertFalse(t, NewDollar(3).Equals(NewFranc(3)))
+}
+
+func TestCurrency(t *testing.T) {
+	testHelper.AssertEqualsPrimitive(t, NewDollar(1).Currency(), "USD")
+	testHelper.AssertEqualsPrimitive(t, NewFranc(1).Currency(), "CHF")
+}
+
+func TestSimpleAddition(t *testing.T) {
+	dollar := NewDollar(3)
+	sum := dollar.Plus(NewDollar(4))
+	bank := Bank{}
+	reduced := bank.Reduce(sum, "USD")
+	testHelper.AssertEquals(t, NewDollar(7), reduced)
+}
+
+func TestReduceSum(t *testing.T) {
+	sum := Sum{augend: 4, addend: 6}
+	bank := Bank{}
+	result := bank.Reduce(sum, "USD")
+	testHelper.AssertEquals(t, result, NewDollar(10))
+}
--- money-tdd/test_helper/assert.go
+++ money-tdd/test_helper/assert.go
@@ -1,32 +1,32 @@
-package money
+package testHelper
 
 import (
 	"reflect"
 	"testing"
 )
 
-func assertEqualsPrimitive[T comparable](t *testing.T, expected, actual T) {
+func AssertEqualsPrimitive[T comparable](t *testing.T, expected, actual T) {
 	t.Helper()
 	if expected != actual {
 		t.Errorf("Assertion failed: Expected %v, but got %v", expected, actual)
 	}
 }
 
-func assertEquals(t *testing.T, expected, actual interface{}) {
+func AssertEquals(t *testing.T, expected, actual interface{}) {
 	t.Helper()
 	if !reflect.DeepEqual(expected, actual) {
 		t.Errorf("Assertion failed (DeepEqual): Expected %v, but got %v", expected, actual)
 	}
 }
 
-func assertTrue(t *testing.T, b bool) {
+func AssertTrue(t *testing.T, b bool) {
 	t.Helper()
 	if !b {
 		t.Errorf("it is false")
 	}
 }
 
-func assertFalse(t *testing.T, b bool) {
+func AssertFalse(t *testing.T, b bool) {
 	t.Helper()
 	if b {
 		t.Errorf("it is true")
